53c4c4bde1e9dc937fd511335fcfcf64d479ccdc
manishaacharya111 <116179205+manishaacharya111@users.noreply.github.com>
2024-06-11 11:07:09 +0200

    Create main.tf

---
+++ Jenkins-Terraform/main.tf
@@ -0,0 +1,45 @@
+resource "aws_security_group" "Jenkins-sg" {
+  name        = "Jenkins-Security Group"
+  description = "Open 22,443,80,8080,9000,9100,9090,3000"
+
+  # Define a single ingress rule to allow traffic on all specified ports
+  ingress = [
+    for port in [22, 80, 443, 8080,3000] : {
+      description      = "TLS from VPC"
+      from_port        = port
+      to_port          = port
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Jenkins-sg"
+  }
+}
+
+resource "aws_instance" "jenkins" {
+  ami                    = "ami-0f58b397bc5c1f2e8"  #change your ami value according to your aws instance
+  instance_type          = "t2.micro"
+  key_name               = "maylatest"
+  vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
+  user_data              = templatefile("./script.sh", {})
+
+  tags = {
+    Name = "jenkins"
+  }
+  root_block_device {
+    volume_size = 15
+  }
+}
