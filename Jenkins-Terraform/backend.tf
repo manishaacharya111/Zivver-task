3e75e8b741cd6b7a3a9a18938321ef501170d44c
manishaacharya111 <116179205+manishaacharya111@users.noreply.github.com>
2024-06-11 11:28:38 +0200

    Update backend.tf

--- Jenkins-Terraform/backend.tf
+++ Jenkins-Terraform/backend.tf
@@ -3,5 +3,6 @@ terraform {
     bucket = "zivver-terraform-s3-bucket" # Provide the actual S3 bucket name created in console
     key    = "zivver/jenkins/terraform.tfstate"
     region = "ap-south-1"
+    dynamodb_table = "dynamodb-state-locking"
   }
 }
