0352a9a7eed2920c90b1408b8e75a327ce6b965e
manishaacharya111 <116179205+manishaacharya111@users.noreply.github.com>
2024-06-11 11:16:27 +0200

    Update backend.tf

--- Jenkins-Terraform/backend.tf
+++ Jenkins-Terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "zivver-bucket" # Provide the actual S3 bucket name created in console
-    key    = "zivver-jenkins/terraform.tfstate"
+    key    = "zivver/jenkins/terraform.tfstate"
     region = "ap-south-1"
   }
 }
